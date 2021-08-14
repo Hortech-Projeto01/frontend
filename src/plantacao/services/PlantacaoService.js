a7593890bd69346c6c8f8aade5a7fc5d042cf0b9
Aislan Jefferson <aislanjsb@gmail.com>
2021-08-14 15:46:43 -0300

    Corrigido typo

--- src/plantacao/services/PlantacaoService.js
+++ src/plantacao/services/PlantacaoService.js
@@ -1,7 +1,7 @@
 import ApiEndpoints from "core/constants/ApiEndpoints";
 import { ServiceBase } from "core/service/ServiceBase";
 
-export default class PlantaService extends ServiceBase {
+export default class PlantacaoService extends ServiceBase {
   constructor() {
     super(ApiEndpoints.plantacoes);
   }
